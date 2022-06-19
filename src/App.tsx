d4b32e04e556828b34b04c94db749306780a7769
Emanuel Lindström <emanuel.lindstrom@creatics.se>
2022-06-19 16:15:13 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const wasm_pdf = import("wasm-pdf");
+
+const testDoc = {
+  title: "Example Document",
+  contents: [
+    {
+      obj_type: "Paragraph",
+      params: {
+        text: "Hello World!",
+        font_size: 18,
+        leading: 24,
+        align: "center",
+        font_name: "Helvetica-Bold",
+      },
+    },
+  ],
+};
+
+const createPDF = (jsDocument: Object) => {
+  wasm_pdf
+    .then((pdf) => {
+      console.log("Calling pdf.run... ");
+      pdf.run(jsDocument);
+    })
+    .catch((e) => {
+      console.error("Error creating pdf: ", e);
+    });
+};
+
+createPDF(testDoc);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
